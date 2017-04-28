f89102cbeb720b24458707cace4725258c6de010
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-28 16:01:38 -0600

    changed file organization to host

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var app = require('express')();
+var express = require('express');
 var srv = require('http').createServer(app);
 var bodyParser = require('body-parser');
 
+
+var app = module.exports = express();
 app.use(bodyParser.json());
-app.use(express.static('./'))
+app.use(express.static('dist'))
 
 app.post('/', function (req, res, next) {
   res.send('You POSTed to the micro-service!');
